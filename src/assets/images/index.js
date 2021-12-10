1a58b09251bc8aaacbda59d93ecef3e48f75fea9
YernarT <toktaryernar@gmail.com>
2021-12-10 23:29:18 +0600

    feat: 图片资源

---
+++ src/assets/images/index.js
@@ -0,0 +1,18 @@
+export { default as logoMain } from './logo-main.svg';
+
+export { default as aitu } from './aitu.svg';
+export { default as did } from './did.svg';
+export { default as dosmart } from './dosmart.svg';
+export { default as kassa24 } from './kassa24.svg';
+export { default as suraqtar } from './suraqtar.svg';
+
+export { default as pix } from './pix.svg';
+
+export { default as team1 } from './team-1.jpg';
+export { default as team3 } from './team-3.png';
+export { default as team4 } from './team-4.jpg';
+export { default as team5 } from './team-5.png';
+export { default as team7 } from './team-7.png';
+export { default as team9 } from './team-9.png';
+
+export { default as map } from './map.png';
