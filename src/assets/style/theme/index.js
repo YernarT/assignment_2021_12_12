0c61f058ad8080a446d59ae3aef0a4647fadab7b
YernarT <toktaryernar@gmail.com>
2021-12-10 23:27:29 +0600

    feat: 主题

---
+++ src/assets/style/theme/index.js
@@ -0,0 +1,11 @@
+import { default as palette } from './palette';
+import { default as typography } from './typography';
+import { default as shape } from './shape';
+
+const theme = {
+	palette,
+	typography,
+	shape,
+};
+
+export default theme;
