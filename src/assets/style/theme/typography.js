a97d1aec61dbf411d1e1ed7b77fabf1ead5d4e9a
YernarT <toktaryernar@gmail.com>
2021-12-10 23:26:52 +0600

    feat: 字体主题

---
+++ src/assets/style/theme/typography.js
@@ -0,0 +1,23 @@
+const typography = {
+	title: {
+		color: '#000',
+		fontSize: '5rem',
+		fontWeight: '900',
+
+		whiteSpace: 'nowrap',
+	},
+
+	text: {
+		color: '#000',
+		fontSize: '2rem',
+		fontWeight: '400',
+	},
+
+	secondary: {
+		color: '#7a7a7a',
+		fontSize: '1.6rem',
+		fontWeight: '400',
+	},
+};
+
+export default typography;
