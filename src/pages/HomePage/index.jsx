563fdff1b9e23e5051a12e6370465517f0153cfe
YernarT <toktaryernar@gmail.com>
2021-12-11 23:45:09 +0600

    update: 引入 Products 组件

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,6 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
-import { LanguageContext } from '../../App';
-import { Navbar } from '../../components';
+import { Navbar, Intro, Products } from '../../components';
 
 import { useTitle } from 'ahooks';
 
@@ -11,13 +10,11 @@ export default function HomePage() {
 	// set <title> Title </title>
 	useTitle('Главная');
 
-	const [lang, setLang] = useContext(LanguageContext);
-	// lang options: ru, en, kz
-
 	return (
 		<HomePageContainer>
 			<Navbar />
-			{/* <Intro /> */}
+			<Intro />
+			<Products />
 		</HomePageContainer>
 	);
 }
