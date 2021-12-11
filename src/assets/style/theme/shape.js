6938ee2e0b6e510b9dd4a8d50059c81ccf3e46c8
YernarT <toktaryernar@gmail.com>
2021-12-11 19:16:39 +0600

    feat: 新增 responsiveWidth

--- src/assets/style/theme/shape.js
+++ src/assets/style/theme/shape.js
@@ -1,8 +1,23 @@
+import { css } from 'styled-components';
+
 const shape = {
 	iconShape: {
 		color: '#91969e',
 		fontSize: '1rem',
 	},
+
+	responsiveWidth: css`
+		/* align center */
+		margin: auto;
+
+		width: 1200px;
+		@media screen and (max-width: 1240px) {
+			width: 85%;
+		}
+		@media screen and (max-width: 1000px) {
+			width: 95%;
+		}
+	`,
 };
 
 export default shape;
