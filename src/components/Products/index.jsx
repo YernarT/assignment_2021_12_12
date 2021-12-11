503be8f7e9feea878cdc39155cb7cb44cfa02fa1
YernarT <toktaryernar@gmail.com>
2021-12-12 00:12:10 +0600

    update: 新增 标题, 文字

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
-import { ProductsContainer } from './style';
+import { circleBackground } from '../../assets/images';
+import { ProductsContainer, CarouselContainer } from './style';
 
 export default function Products() {
-	return <ProductsContainer id="products"></ProductsContainer>;
+	return (
+		<ProductsContainer id="products">
+			<img
+				src={circleBackground}
+				alt="cirle-background"
+				className="cirle-background"
+			/>
+
+			<h1 className="title">Продукты</h1>
+			<p className="text">2,6 миллиона пользователей уже выбрали нас</p>
+			<Carousel />
+		</ProductsContainer>
+	);
+}
+
+function Carousel() {
+	return <CarouselContainer></CarouselContainer>;
 }
--- src/components/Products/style.js
+++ src/components/Products/style.js
@@ -2,5 +2,37 @@ import styled from 'styled-components';
 
 export const ProductsContainer = styled.section`
 	${({ theme }) => theme.shape.responsiveWidth};
-    margin-top: 65px;
+	margin-top: 65px;
+	padding-bottom: 65px;
+
+	position: relative;
+
+	display: flex;
+	flex-direction: column;
+
+	.cirle-background {
+		position: absolute;
+		top: 0;
+		left: 0;
+		z-index: -1;
+
+		width: 200%;
+		height: 200%;
+        object-fit: cover;
+	}
+
+	.title,
+	.text {
+		margin-bottom: 20px;
+	}
+
+	.title {
+		${({ theme }) => theme.typography.title};
+	}
+
+	.text {
+		${({ theme }) => theme.typography.text};
+	}
 `;
+
+export const CarouselContainer = styled.div``;
