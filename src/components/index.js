e56c2c153c761b6eb7f9e9abe20d865f2f733ebe
YernarT <toktaryernar@gmail.com>
2021-12-13 00:31:47 +0600

    update: 导出所有页面内组件

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,11 @@ export { default as Navbar } from './Navbar';
 export { default as Intro } from './Intro';
 
 export { default as Products } from './Products';
+
+export { default as Team } from './Team';
+
+export { default as ChangeWorld } from './ChangeWorld';
+
+export { default as Vacancies } from './Vacancies';
+
+export { default as Contact } from './Contact';
